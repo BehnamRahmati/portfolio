af40839bad6bcfaf2ebd056d7174780ac0a8f941
behnam.rahmati <behnam.main@gmail.com>
2022-10-11 16:18:05 +0330

    scrollbar styles

--- src/components/lists/primary_list/PrimaryList.tsx
+++ src/components/lists/primary_list/PrimaryList.tsx
@@ -2,31 +2,25 @@ import styles from "./PrimaryList.module.css";
 
 export interface IPrimaryList {
 	title: string;
-	count:number;
-	list: {info_item : string;}[]
+	count: number;
+	list: { info_item: string }[];
 }
 
-const PrimaryList: React.FC<IPrimaryList> = ({
-	title,
-	count,
-	list
-}) => {
-	return <div className={styles.listContainer}>
-		<div>
-			<span>0{count}</span>
-			<h3>{title}</h3>
-		</div>
+const PrimaryList: React.FC<IPrimaryList> = ({ title, count, list }) => {
+	return (
+		<div className={styles.listContainer}>
+			<div>
+				<span>0{count}</span>
+				<h3>{title}</h3>
+			</div>
 
-		<ul className='ltr'>
-			{
-				list.map((item , i) => {
-					return(
-						<li key={item.info_item}>{item.info_item}</li>
-					)
-				})
-			}
-		</ul>
-	</div>;
+			<ul className="ltr">
+				{list.map((item, i) => {
+					return <li key={item.info_item}>{item.info_item}</li>;
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default PrimaryList;
